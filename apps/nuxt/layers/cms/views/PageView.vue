549043394121c46b4557636a9bef13ed8e8c7f8e
Robbe <robbevaes95@gmail.com>
2024-11-25 13:52:41 +0100

    feat: fix page

--- apps/nuxt/layers/cms/views/PageView.vue
+++ apps/nuxt/layers/cms/views/PageView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { useTrpc } from '@base/composables/api/useTrpc'
 import { usePageQuery } from '@cms/api/query/usePageQuery'
 import { useLivePreview } from '@cms/composables/useLivePreview'
 
@@ -9,15 +8,6 @@ const route = useRoute('page___en')
 
 const pageName = computed<string>(() => route.params.page)
 const pageQuery = usePageQuery({ slug: pageName.value })
-const trpc = useTrpc()
-
-async function getPage() {
-  const data = await trpc.pages.getPageBySlug.query({ slug: pageName.value })
-
-  return data ?? null
-}
-
-await getPage()
 
 const { data } = useLivePreview({
   initialData: pageQuery.data,
